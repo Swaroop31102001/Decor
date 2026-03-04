3a334971c1d143a88639ab877fd30ec7555cd13f
Chanthani-N-R-31 <chanthanirajiraj2003@gmail.com>
2026-03-04 11:10:10 +0530

    update

--- app.py
+++ app.py
@@ -14,5 +14,29 @@ def home():
 def gallery():
     return render_template("gallery.html")
 
+@app.route("/services")
+def services():
+    return render_template("index.html")
+
+@app.route("/activities")
+def activities():
+    return render_template("index.html")
+
+@app.route("/interactive")
+def interactive():
+    return render_template("index.html")
+
+@app.route("/snacks")
+def snacks():
+    return render_template("index.html")
+
+@app.route("/flower-decor")
+def flower_decor():
+    return render_template("index.html")
+
+@app.route("/contact")
+def contact():
+    return render_template("index.html")
+
 if __name__ == "__main__":
     app.run(debug=True)
